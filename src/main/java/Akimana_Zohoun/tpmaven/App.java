79b0abbba700e4d02a73558db99b8dcb4432b90a
18012887 <18012887>
2018-01-08 18:04:39 +0100

    first commit

---
+++ src/main/java/Akimana_Zohoun/tpmaven/App.java
@@ -0,0 +1,25 @@
+package Akimana_Zohoun.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private int somme= 0;
+	
+	public void setSOMME(int a ,int b){
+		somme = a + b;
+	}
+	public int getSOMME(){
+		return somme;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void setSOmMME(int a ,int b){
+		somme = a + b;
+	}
+    
+}
